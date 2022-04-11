e3aa6229bee2d22008204a986a57e6f1c45cd000
Dita Permata Putra <ditateum@gmail.com>
2022-04-11 17:14:17 +0700

    sign up page done

--- lib/view/form/sign_up_page.dart
+++ lib/view/form/sign_up_page.dart
@@ -6,9 +6,71 @@ class SignUpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: const Center(
-        child: Text('Sign Up'),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Sign Up",
+              style: TextStyle(
+                fontSize: 22,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(
+              height: 26,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Your Name'),
+            ),
+            const SizedBox(
+              height: 26,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Your Email'),
+            ),
+            const SizedBox(
+              height: 26,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Your Password'),
+            ),
+            Container(
+              width: double.infinity,
+              height: 45,
+              margin: const EdgeInsets.symmetric(
+                vertical: 26,
+              ),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.amber,
+                  primary: Colors.black,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+                child: const Text('Sign Up'),
+              ),
+            ),
+            Center(
+              child: TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'Have account? sign in',
+                  style: TextStyle(
+                      color: Colors.black45,
+                      decoration: TextDecoration.underline),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
